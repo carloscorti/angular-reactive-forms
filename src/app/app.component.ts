af1a61815fb732620e81cc43c8153251c75c5dc7
Carlos Corti <carli@Carloss-MacBook-Pro.local>
2022-05-31 18:10:52 +0200

    adding form control groups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.signUpFrom = new FormGroup({
-      username: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
       gender: new FormControl('male'),
     });
   }
